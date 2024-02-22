c2cbc179585bfad0ad0953807324742edfc79a5a
Fernando <Fernando@DESKTOP-9JIAMII>
2024-02-22 07:52:35 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnica02FJMAJueves22/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaTecnica02FJMAJueves22.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
