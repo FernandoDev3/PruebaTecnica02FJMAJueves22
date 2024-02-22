ce704216aa674b27288e85c84d80e21dffdbba1b
fernando13522477@outlook.com <fernando13522477@outlook.com>
2024-02-22 08:42:04 -0600

    Vistas: Create, Delete, Details, Edit, Index: Creadas con el controlador.

---
+++ PruebaTecnica02FJMAJueves22/Models/PruebaTecnica02FJMAContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PruebaTecnica02FJMAJueves22.Models;
+
+namespace PruebaTecnica02FJMAJueves22.Models
+{
+    public partial class PruebaTecnica02FJMAContext : DbContext
+    {
+        public PruebaTecnica02FJMAContext()
+        {
+        }
+
+        public PruebaTecnica02FJMAContext(DbContextOptions<PruebaTecnica02FJMAContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /*if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-9JIAMII;Initial Catalog=PruebaTecnica02FJMA;Integrated Security=True;Trust Server Certificate=True");
+            }*/
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<PruebaTecnica02FJMAJueves22.Models.Marcas>? Marcas { get; set; }
+    }
+}
