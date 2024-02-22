a7c2ef4bf1fa3f078c12d245b5602a3f47072421
fernando13522477@outlook.com <fernando13522477@outlook.com>
2024-02-22 08:02:49 -0600

    Clase marcas creada

---
+++ PruebaTecnica02FJMAJueves22/Models/Marcas.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PruebaTecnica02FJMAJueves22.Models
+{
+    public class Marcas
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [MinLength(5), MaxLength(30)]
+        public string Nombre { get; set; }
+    }
+}
