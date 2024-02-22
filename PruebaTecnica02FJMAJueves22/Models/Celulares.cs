ed052d79cc013cd9fc4d86fa0079f7a80d9b1e75
fernando13522477@outlook.com <fernando13522477@outlook.com>
2024-02-22 08:14:23 -0600

    Clase Celulares creada, Modificacion a Marcas

---
+++ PruebaTecnica02FJMAJueves22/Models/Celulares.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnica02FJMAJueves22.Models
+{
+    public class Celulares
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio {  get; set; }
+        public byte[]? Imagen {  get; set; }
+        [Display(Name = "Marca")]
+        public int MarcaId { get; set; }
+
+        public virtual Marcas Marcas { get; set; } = null!;
+    }
+}
--- PruebaTecnica02FJMAJueves22/Models/Marcas.cs
+++ PruebaTecnica02FJMAJueves22/Models/Marcas.cs
@@ -5,11 +5,18 @@ namespace PruebaTecnica02FJMAJueves22.Models
 {
     public class Marcas
     {
+        public Marcas()
+        {
+            Celular = new HashSet<Celulares>();
+        }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es obligatorio")]
         [MinLength(5), MaxLength(30)]
         public string Nombre { get; set; }
+
+        public virtual ICollection<Celulares> Celular { get; set; }
     }
 }
